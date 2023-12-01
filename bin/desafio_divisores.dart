dc347de4d062d65902aa93537f0acdde75820ee6
Erica Rodrigues <ericamrodrigs@gmail.com>
2023-12-01 16:38:05 -0300

    refactor: :art: melhora mensagem de retorno para o usuário

--- bin/desafio_divisores.dart
+++ bin/desafio_divisores.dart
@@ -6,7 +6,8 @@ void main() {
   int? numeroLimite = int.parse(stdin.readLineSync()!);
 
   int resultado = somaDivisores3ou5(numeroLimite);
-  print(resultado);
+  print(
+      'A soma de todos os divisores de 3 ou de 5 que são menores que $numeroLimite é: $resultado');
 }
 
 int somaDivisores3ou5(int numeroLimite) {
